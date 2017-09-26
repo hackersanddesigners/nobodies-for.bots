516d1bf8a37b44df531874055bb6c00c1211ff3b
Cristina <franz.andersson@gmail.com>
2017-09-27 00:21:47 +0200

    simple trigger and response IRC bot

---
+++ ellen.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 =-*-
+
+ellen = [
+'The programmer, who needs clarity, who must talk all day to a machine that demands declarations, hunkers down into a low-grade annoyance.',
+'It is here that the stereotype of the programmer, sitting in a dim room, growling from behind Coke cans, has its origins.',
+'The disorder of the desk, the floor; the yellow Post-it notes everywhere; the whiteboards covered with scrawl: all this is the outward manifestation of the messiness of human thought.',
+'The messiness cannot go into the program; it piles up around the programmer.',
+
+'Soon the programmer has no choice but to retreat into some private interior space, closer to the machine, where things can be accomplished.',
+'The machine begins to seem friendlier than the analysts, the users, the managers. The real-world reflection of the program — who cares anymore?',
+'Guide an x-ray machine or target a missile; print a budget or a dossier; run a city subway or a disk-drive read/write arm: it all begins to blur.',
+'The system has crossed the membrane — the great filter of logic, instruction by instruction — where it has been cleansed of its linkages to actual human life.',
+
+'The goal now is not whatever all the analysts first set out to do; the goal becomes the creation of the system itself.',
+'Any ethics or morals or second thoughts, any questions or muddles or exceptions, all dissolve into a junky Nike-mind: Just do it.',
+'If I just sit here and code, you think, I can make something run.',
+'When the humans come back to talk changes, I can just run the program.',
+'Show them: Here. Look at this. See? This is not just talk. This runs.',
+'Whatever you might say, whatever the consequences, all you have are words and what I have is this, this thing I’ve built, this operational system.',
+'Talk all you want, but this thing here: it works.'
+]
